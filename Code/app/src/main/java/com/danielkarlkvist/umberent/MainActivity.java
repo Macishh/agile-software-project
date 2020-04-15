27ff4e0b83323d75d4a0ed7560be8c53aab3c65a
Daniel Karlkvist <danielkarlkvist@gmail.com>
2020-04-15 18:40:26 +0200

    App is now only in portrait mode

--- Code/app/src/main/java/com/danielkarlkvist/umberent/MainActivity.java
+++ Code/app/src/main/java/com/danielkarlkvist/umberent/MainActivity.java
@@ -2,6 +2,7 @@ package com.danielkarlkvist.umberent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,7 +11,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);  // Always portrait mode
 
         hideNavigationBar();
     }
