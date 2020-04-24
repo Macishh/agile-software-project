d82d2bfa1d2f1ae8c1258903f108c2998dbf0747
Marcus Axelsson <marcusaxelsson52@gmail.com>
2020-04-24 17:07:12 +0200

    Added AccountFragment and so the menu item is connected to it

--- Umberent/app/src/main/java/com/danielkarlkvist/umberent/MainActivity.java
+++ Umberent/app/src/main/java/com/danielkarlkvist/umberent/MainActivity.java
@@ -1,16 +1,23 @@
 package com.danielkarlkvist.umberent;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
     private Toolbar toolbar;
     private MapFragment mapFragment = new MapFragment();
 
@@ -23,7 +30,24 @@ public class MainActivity extends AppCompatActivity {
         initializeViews();
         setSupportActionBar(toolbar);
         addToggleForToolbar();
+        setNavigationListener();
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, mapFragment).commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_account:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AccountFragment()).commit();
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void setNavigationListener() {
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     // adds a toggle for the toolbar so that when it is tapped, the side menu will pop out
@@ -36,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
     // finds the correct view in the xml file and connects it to the instance variables
     private void initializeViews() {
         drawerLayout = findViewById(R.id.test1);
+        navigationView = findViewById(R.id.nav_view);
         toolbar = findViewById(R.id.toolbar1);
     }
 
@@ -49,4 +74,6 @@ public class MainActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
     }
+
+
 }
