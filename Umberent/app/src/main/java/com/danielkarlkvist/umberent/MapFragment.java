ac05ead08bd54924a15705c152a260a101f42e3c
Robin Repo Wecklauf <robinrepowecklauf@gmail.com>
2020-04-29 14:12:53 +0200

    add stand card for the popup window that shows up when clicking a marker
    Co-authored-by: Johan Gottlander <johangottlander@gmail.com>

--- Umberent/app/src/main/java/com/danielkarlkvist/umberent/MapFragment.java
+++ Umberent/app/src/main/java/com/danielkarlkvist/umberent/MapFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -17,6 +19,8 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 
 
 /**
@@ -55,13 +59,45 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleM
         mMap.setOnMarkerClickListener(this);
     }
 
+  /*  public void onButtonShowPopupWindowClick(View view) {
+
+        // inflate the layout of the popup window
+
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
+    } */
+
     // Called when the user clicks a marker.
     @Override
     public boolean onMarkerClick(final Marker marker) {
 
         // Retrieve the data from the marker.
-        if(marker.getTitle().equals("9/10")){
-            Toast.makeText(getActivity().getApplicationContext(), "5 kr / min", Toast.LENGTH_SHORT).show();
+        if(marker.getTitle().equals("Emilsborg")){
+            //Toast.makeText(getActivity().getApplicationContext(), "5 kr / min", Toast.LENGTH_SHORT).show();
+            LayoutInflater inflater = (LayoutInflater)
+                    getActivity().getSystemService(LAYOUT_INFLATER_SERVICE);
+            View popupView = inflater.inflate(R.layout.stand_card, null);
+
+            PopupWindow pop = new PopupWindow(popupView);
+            //PopupWindow pop = new PopupWindow(inflater.inflate(R.layout.stand_card, null, false), true);
+            pop.showAtLocation(popupView, Gravity.BOTTOM, 40, 40);
 
         }
 
